f377dc2e9b846bd38170c446dcd2b9cd3442bfa8
Max Lynch <max@ionic.io>
2021-10-20 20:20:04 -0500

    Bringing back manifest

--- packages/configure/src/operations/android/manifest.ts
+++ packages/configure/src/operations/android/manifest.ts
@@ -1,8 +1,13 @@
 import { Context } from '../../ctx';
 import { Operation } from '../../definitions';
 
-export default async function execute(_ctx: Context, op: Operation) {
+export default async function execute(ctx: Context, op: Operation) {
   // const android = ctx.project.android;
 
-  console.log(op);
+  if (op.value.attrs) {
+    ctx.project.android.getAndroidManifest().setAttrs(op.value.target, op.value.attrs)
+  }
+  if (op.value.inject) {
+    ctx.project.android.getAndroidManifest().injectFragment(op.value.target, op.value.inject);
+  }
 }
