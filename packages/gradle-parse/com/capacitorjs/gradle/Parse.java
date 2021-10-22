9ea08f9470252ab6022e4df507f952fc20d7bdcf
Max Lynch <max@ionic.io>
2021-10-22 14:06:46 -0500

    Comments for gradle parser

--- packages/gradle-parse/com/capacitorjs/gradle/Parse.java
+++ packages/gradle-parse/com/capacitorjs/gradle/Parse.java
@@ -13,6 +13,16 @@ import java.util.Map;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+/**
+ * A utility for building a limited AST of a given Gradle/Groovy file. This AST
+ * *does not* contain adequate data to reconstruct the file accurately. Instead,
+ * it's used to correctly track gradle methods including nested ones along with
+ * source location information, to make it easy to apply basic string injection
+ * to modify the file without breaking the structure of the source file.
+ *
+ * It currently only handles Block statements, Closures, and Methods and ignores
+ * all other AST Nodes
+ */
 public class Parse {
   public static void main(String[] args) {
     try {
@@ -49,19 +59,16 @@ class Visitor {
       if (node instanceof BlockStatement) {
         children.put(visitBlockStatement((BlockStatement) node));
       }
-      // System.out.println(node.getClass());
     }
   }
 
-  private void log(String s) {
-    System.out.println(s);
-  }
-
+  /**
+   * a block statement contains a list of other statements. For example, the top level of
+   * the file is a Block with a statement for each Gradle construct
+   */
   private JSONObject visitBlockStatement(BlockStatement block) {
     JSONObject jsonNode = new JSONObject();
     JSONArray jsonChildren = new JSONArray();
-
-    // log("Visiting " + block.getStatements().size());
 
     for (Statement statement : block.getStatements()) {
       if (statement instanceof ExpressionStatement) {
@@ -78,11 +85,11 @@ class Visitor {
     return jsonNode;
   }
 
+  /**
+   * Upon finding an Expression, visit that and look for Method calls (such as buildscript or dependencies)
+   */
   private JSONObject visitExpressionStatement(ExpressionStatement exprStatement) {
     Expression expr = exprStatement.getExpression();
-
-    // log("Visiting expression " + expr.getClass());
-
 
     if (expr instanceof MethodCallExpression) {
       MethodCallExpression mcExpr = (MethodCallExpression) expr;
@@ -93,26 +100,27 @@ class Visitor {
     return null;
   }
 
+  /**
+   * A method call is what we're looking for. These are the primary Gradle constructs in the Groovy DSL
+   * that makes up Gradle: https://docs.gradle.org/current/dsl/index.html
+   */
   private JSONObject visitMethodCallExpression(ExpressionStatement exprStatement, MethodCallExpression mcExpr) {
     JSONObject jsonNode = new JSONObject();
     JSONArray children = new JSONArray();
-
-    // log(mcExpr.getMethodAsString());
-
-    // Expression args = mcExpr.getArguments();
-    // log("Args: " + args);
-    // List<Expression> expressions = mcExpr.getExpressions();
-    ConstantExpression method = (ConstantExpression) mcExpr.getMethod();
     addSourceInfo(exprStatement, jsonNode);
+
+    ConstantExpression method = (ConstantExpression) mcExpr.getMethod();
     String methodName = (String) method.getValue();
 
+    // Get the "arguments" for the MethodCallExpression, which are a list of expressions
+    // passed to the method, such as a nested Closure expression (aka a method body)
     TupleExpression args = (TupleExpression) mcExpr.getArguments();
 
     List<Expression> expressions = args.getExpressions();
 
     for (Expression ex : expressions) {
-      // log("Expression: " + ex.getClass());
       if (ex instanceof ClosureExpression) {
+        // Descend into the closure expression to handle nested method calls
         children.put(visitClosureExpression((ClosureExpression) ex));
       }
     }
@@ -123,6 +131,10 @@ class Visitor {
     return jsonNode;
   }
 
+  /**
+   * Visit a Closure expression (the body of a method call) and descend into any block statements,
+   * this enables us to recursively define the basic structure of the Gradle file
+   */
   private JSONObject visitClosureExpression(ClosureExpression closureExpr) {
     JSONObject jsonNode = new JSONObject();
     Statement code = closureExpr.getCode();
@@ -131,10 +143,12 @@ class Visitor {
       return visitBlockStatement((BlockStatement) code);
     }
 
-    // log("Closure code: " + code.getClass());
     return jsonNode;
   }
 
+  /**
+   * Apply source location information for the ASTNode to the collector JSONObject
+   */
   private void addSourceInfo(ASTNode node, JSONObject jsonNode) {
     JSONObject source = new JSONObject();
     source.put("line", node.getLineNumber());
@@ -143,4 +157,9 @@ class Visitor {
     source.put("lastColumn", node.getLastColumnNumber());
     jsonNode.put("source", source);
   }
+
+  private void log(String s) {
+    System.out.println(s);
+  }
+
 }
