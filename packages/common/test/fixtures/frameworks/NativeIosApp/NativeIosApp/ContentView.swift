8eb9b0f695834929edc52286e994bb92a1f30ad9
Max Lynch <max@ionic.io>
2022-05-18 14:38:34 -0500

    Native iOS and Android fixtures apps

---
+++ packages/common/test/fixtures/frameworks/NativeIosApp/NativeIosApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NativeIosApp
+//
+//  Created by Max Lynch on 5/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
