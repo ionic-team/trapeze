386186fa926e7b8f234818607edd04f914cfa8d4
Max Lynch <max@ionic.io>
2022-06-06 12:10:35 -0500

    Bad website link

--- packages/website/docusaurus.config.js
+++ packages/website/docusaurus.config.js
@@ -82,8 +82,8 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Overview',
+                to: '/docs/',
               },
             ],
           },
