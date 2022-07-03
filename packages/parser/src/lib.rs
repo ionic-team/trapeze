20f3c7405b330f042caa78cc4e69d896fbcc474d
Max Lynch <max@ionic.io>
2022-07-03 17:51:15 -0500

    Kotlin script

--- packages/parser/src/lib.rs
+++ packages/parser/src/lib.rs
@@ -3,19 +3,61 @@ use crate::kotlin::*;
 use crate::kotlinlexer::KotlinLexer;
 use antlr_rust::InputStream;
 use antlr_rust::common_token_stream::CommonTokenStream;
-use kotlin::kotlinparser::KotlinParser;
+use antlr_rust::tree::{ParseTree, ParseTreeListener};
+use kotlin::kotlinparser::{KotlinParser, KotlinParserContextType, KotlinParserContext};
+use kotlin::kotlinparserlistener::KotlinParserListener;
+
+struct Listener {}
+
+impl<'input> ParseTreeListener<'input, KotlinParserContextType> for Listener {
+    fn enter_every_rule(&mut self, ctx: &dyn KotlinParserContext<'input>) {
+        println!(
+            "rule entered {}",
+            kotlinparser::ruleNames
+                .get(ctx.get_rule_index())
+                .unwrap_or(&"error")
+        )
+    }
+}
+
+impl<'input> KotlinParserListener<'input> for Listener {}
+
 
 pub fn parse() {
     // KotlinParser::with_strategy();
-    let input = r#"package org.kotlinlang.play
-fun main() {
-    println("Hello, World!")
+    let input = r#"
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.5.31" 
+
+    application 
+}
+
+repositories {
+    mavenCentral() 
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom")) 
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8") 
+
+    implementation("com.google.guava:guava:30.1.1-jre") 
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test") 
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit") 
+}
+
+application {
+    mainClass.set("demo.AppKt") 
 }"#.to_owned();
 
     let lexer = KotlinLexer::new(InputStream::new(&*input));
     let token_source = CommonTokenStream::new(lexer);
     let mut parser = KotlinParser::new(token_source);
-    let result = parser.kotlinFile();
+    parser.add_parse_listener(Box::new(Listener {}));
+    let result = parser.script();
+    // println!("{}", result.unwrap())
     println!("Parsed file");
 }
 
