f1d4fd24dc7aa5bd29e5e77d08e96c9944a9d96d
Max Lynch <max@ionic.io>
2022-11-15 15:32:22 -0600

    Starting to support non-platform scoped operations #107

---
+++ packages/configure/src/operations/project/json.ts
@@ -0,0 +1,37 @@
+import { join } from 'path';
+
+import { JsonFile, VFS } from '@trapezedev/project';
+import { Context } from '../../ctx';
+import { JsonOperation, Operation } from '../../definitions';
+
+function getJsonFile(path: string, vfs: VFS) {
+  const existing = vfs.get(path);
+
+  if (existing) {
+    return existing.getData() as JsonFile;
+  }
+  return new JsonFile(path, vfs);
+}
+
+export default async function execute(ctx: Context, op: Operation) {
+  const jsonOp = op as JsonOperation;
+  const entries = jsonOp.value;
+
+  for (const entry of entries) {
+    let filename = entry.file;
+    if (!filename) {
+      continue;
+    }
+
+    let jsonFile: JsonFile = getJsonFile(join(ctx.project.projectRoot, filename), ctx.project.vfs);
+
+    await jsonFile.load();
+
+    if (entry.set) {
+      jsonFile.set(entry.set);
+    }
+    if (entry.merge) {
+      jsonFile.merge(entry.merge);
+    }
+  }
+}
