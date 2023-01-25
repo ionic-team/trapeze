08d007f99eaea5c73fc61ffa1544d6be774dabf0
Max Lynch <max@ionic.io>
2023-01-25 12:31:27 -0600

    Set javac --release

--- packages/gradle-parse/build.sh
+++ packages/gradle-parse/build.sh
@@ -3,18 +3,18 @@
 case "$(uname -s)" in
 
    Darwin)
-      javac -cp lib/*:. com/capacitorjs/gradle/Parse.java
+      javac --release 11 -cp lib/*:. com/capacitorjs/gradle/Parse.java
       jar cmvf META-INF/MANIFEST.MF capacitor-gradle-parse.jar com/capacitorjs/gradle/*.class
      ;;
 
    Linux)
-      javac -cp lib/*:. com/capacitorjs/gradle/Parse.java
+      javac --release 11 -cp lib/*:. com/capacitorjs/gradle/Parse.java
       jar cmvf META-INF/MANIFEST.MF capacitor-gradle-parse.jar com/capacitorjs/gradle/*.class
      ;;
 
    CYGWIN*|MINGW32*|MSYS*|MINGW*)
       echo "Windows"
-      javac -cp "lib/groovy-3.0.9.jar;lib/json-20210307.jar;." com\\capacitorjs\\gradle\\Parse.java
+      javac --release 11 -cp "lib/groovy-3.0.9.jar;lib/json-20210307.jar;." com\\capacitorjs\\gradle\\Parse.java
       jar cmvf META-INF/MANIFEST.MF capacitor-gradle-parse.jar com/capacitorjs/gradle/*.class
      ;;
 
